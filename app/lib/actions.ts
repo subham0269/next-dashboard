bc6a6e4f31a41b199ef75d1b3e0d180b9993b35a
subham0269 <gitsubham21@gmail.com>
2024-02-12 23:21:11 +0530

    Day 3: Upto Lesson 12 - Deleting an invoice from the table

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,12 +18,7 @@ const CreateInvoice = formSchema.omit({id: true, date: true});
 const UpdateInvoice = formSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
-    // const rawFormData = {
-    //     customerId: formData.get('customerId'),
-    //     amount: formData.get('amount'),
-    //     status: formData.get('status'),
-    // }
-    const rawFormData = Object.fromEntries(formData.entries());
+    // const rawFormData = Object.fromEntries(formData.entries());
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -54,4 +49,9 @@ export async function updateInvoice (id: string, formData: FormData) {
     `;
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
